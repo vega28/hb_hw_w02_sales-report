424d9d81c60807122f5bcce181c8f06f46932457
Kelsi Flatland <kelsi.flatland@gmail.com>
2021-03-03 01:37:03 +0000

    Refactored sales_report.py. Now includes a more effective data structure and an alphabetized report.

--- sales_report.py
+++ sales_report.py
@@ -1,36 +1,26 @@
 """Generate sales report showing total melons each salesperson sold."""
 
-# initialize lists to store info for salespeople and their melons sold:
-salespeople = []
-melons_sold = []
 
-f = open('sales-report.txt') # open report file
+sales_data = {} # dict datatype for better organization of associated data
+
+# read in file & parse data:
+f = open('sales-report.txt') 
 for line in f:
     line = line.rstrip() # remove whitespace and end-line chars
-    entries = line.split('|') # parse line: split at |s and put in a list
-    # assign variables for specific data:
-    salesperson = entries[0] 
-    melons = int(entries[2])
+    salesperson, sale_amt, melons = line.split('|') # parse line
 
-    # check if the salesperson is already listed
-    if salesperson in salespeople:
-        # determine the index of that entry to compare to melons_sold
-        position = salespeople.index(salesperson)
-        # add the new melons sold to the melons list
-        melons_sold[position] += melons
-    else: 
-        # add salesperson and melon count to the end of their respective lists
-        salespeople.append(salesperson)
-        melons_sold.append(melons)
+    # update dict with this sale info:
+    sales_data[salesperson] = sales_data.get(salesperson, 0) + int(melons)
 
-# print out report of salespeople and how many melons each sold:
-for i in range(len(salespeople)):
-    print(f'{salespeople[i]} sold {melons_sold[i]} melons')
+# print out alphabetized report of salespeople and how many melons each sold:
+for salesperson in sorted(sales_data): 
+    print(f'{salesperson} sold {sales_data[salesperson]} melons')
 
 
-# --- NTH improvements --- #
+# --- NTH improvements - ALL COMPLETE --- #
 # store salespeople and their associated melons in a dict
 #   keeps associated data together! removes possible errors related to indexing
 #   easier to add/update melons in this format
 # when reading in file: unpack each line directly into variables
 #   mostly a style thing, but more elegant!
+# alphabetize report 
